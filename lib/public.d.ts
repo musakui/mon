494ab96a1669d84118609aa57e0201f8cbff4bd0
randName <kwokshungit@gmail.com>
2024-06-22 21:58:36 +0800

    add type generation

---
+++ lib/public.d.ts
@@ -0,0 +1,21 @@
+export * from './index'
+
+export {
+	ColType,
+	ColInfo,
+	ForeignKeyInfo,
+	ForeignKeyAction,
+	FailureAction,
+	TableSchema,
+	DatabaseSchema,
+	QueryColOption,
+	RawCondition,
+	QueryCondition,
+	JoinType,
+	QueryJoinOption,
+	QuerySortOption,
+	SelectStatementOptions,
+	InsertStatementOptions,
+	UpdateStatementOptions,
+	DeleteStatementOptions,
+} from './types'
