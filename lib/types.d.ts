cfeaa3d3eee367c13e013fd02b67d0341517ad6e
randName <kwokshungit@gmail.com>
2024-06-29 17:44:57 +0800

    add query generation

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -90,3 +90,148 @@ export type TableSchema = {
 }
 
 export type DatabaseSchema = Map<string, TableSchema>
+
+// CORE
+
+export type Operation = [op: string, ...unknown[]]
+
+export type ProcessOperation = (
+	op: string,
+	params: string | unknown[]
+) => Operation
+
+export type QueryColOption = {
+	/** selection (e.g. col name) */
+	sel: string
+
+	/** function on value */
+	fn?: string
+
+	/** alias for selection */
+	name?: string
+
+	/** table of sel */
+	table?: string
+}
+
+/** condition with parameters */
+export type RawCondition = {
+	/** raw SQL, or col name / alias if operator provided */
+	sql: string
+
+	/** operator */
+	op?: string
+
+	/** parameters */
+	params?: string | unknown[]
+}
+
+export type BaseCondition = {
+	/** raw SQL */
+	sql: string
+
+	/** parameters */
+	params: unknown[]
+}
+
+/**
+ * WHERE conditions
+ *
+ * each level of nesting will flip between AND and OR
+ */
+export type QueryCondition = string | RawCondition | QueryCondition[]
+
+export type JoinType = 'INNER' | 'LEFT' | 'RIGHT' | 'FULL'
+
+export type QueryJoinOption = {
+	/** other table name */
+	join: string
+
+	/** column name in other table (NATURAL join will be used if unspecified) */
+	name?: string
+
+	/** column name to match (default to name) */
+	col?: string
+
+	/** table name to match (default to current table) */
+	table?: string
+
+	/** custom join condition */
+	cond?: string
+
+	/** join type */
+	type?: JoinType
+}
+
+export type QuerySortOption<ColName extends string = string> = {
+	/** column name */
+	col: ColName
+
+	/** ORDER BY DESC */
+	desc?: boolean
+
+	/** NULLS FIRST */
+	nullsFirst?: boolean
+}
+
+export type SelectStatementOptions = {
+	/** table name */
+	table: string
+
+	/** DISTINCT */
+	distinct?: boolean
+
+	/** columns to select */
+	select?: (string | QueryColOption)[]
+
+	/** additional things to select */
+	addSelect?: QueryColOption[]
+
+	/** WHERE */
+	where?: QueryCondition
+
+	/** JOIN */
+	join?: QueryJoinOption[]
+
+	/** GROUP BY */
+	group?: QueryColOption[]
+
+	/** HAVING */
+	having?: QueryCondition
+
+	/** ORDER BY */
+	sort?: QuerySortOption[]
+
+	/** LIMIT */
+	take?: number
+
+	/** OFFSET */
+	skip?: number
+}
+
+export type InsertStatementOptions = {
+	/** table name */
+	table: string
+
+	/** columns to insert */
+	cols?: string[]
+
+	/** list of rows to insert, one array per row */
+	values: unknown[][]
+
+	/**
+	 * true: ON CONFLICT DO NOTHING
+	 */
+	upsert?: boolean
+
+	/** alternate action upon failure */
+	action?: 'ABORT' | 'FAIL' | 'IGNORE' | 'REPLACE' | 'ROLLBACK'
+}
+
+export type DeleteStatementOptions = {
+	/** table name */
+	table: string
+
+	/** WHERE conditions */
+	where?: QueryCondition
+}
--- test/parse.test.ts
+++ test/parse.test.ts
@@ -16,6 +16,141 @@ describe('getInt', () => {
 	})
 })
 
+describe('getAll', () => {
+	it('processes correctly', ({ expect }) => {
+		const process = (a: string) => (a ? `a${a}` : null)
+		expect(parse.getAll(usp('?'), 'a', process)).toEqual([])
+		expect(parse.getAll(usp('?a=1&a='), 'a', process)).toEqual(['a1'])
+		expect(parse.getAll(usp('?a=1&a=2'), 'a', process)).toEqual(['a1', 'a2'])
+	})
+})
+
+describe('parseSelectOption', () => {
+	it('parses empty', ({ expect }) => {
+		expect(parse.parseSelectOption('')).toEqual(null)
+		expect(parse.parseSelectOption(':')).toEqual(null)
+	})
+
+	it('parses simple', ({ expect }) => {
+		expect(parse.parseSelectOption('foo')).toEqual({ sel: 'foo' })
+	})
+
+	it('parses with function/alias', ({ expect }) => {
+		expect(parse.parseSelectOption('foo:MAX')).toEqual({
+			sel: 'foo',
+			fn: 'MAX',
+		})
+		expect(parse.parseSelectOption('foo:MAX:max')).toEqual({
+			sel: 'foo',
+			fn: 'MAX',
+			name: 'max',
+		})
+		expect(parse.parseSelectOption('foo::bar')).toEqual({
+			sel: 'foo',
+			name: 'bar',
+		})
+	})
+})
+
+describe('parseConditionOption', () => {
+	it('parses empty', ({ expect }) => {
+		expect(parse.parseConditionOption('')).toEqual(null)
+		expect(parse.parseConditionOption(',')).toEqual(null)
+	})
+	it('parses single condition', ({ expect }) => {
+		expect(parse.parseConditionOption('foo:2')).toEqual([
+			{ sql: 'foo', op: '=', params: '2' },
+		])
+		expect(parse.parseConditionOption('bar__le:420')).toEqual([
+			{ sql: 'bar', op: '<=', params: '420' },
+		])
+		expect(parse.parseConditionOption('baz__nu:_')).toEqual([
+			{ sql: 'baz', op: 'ISNULL', params: '_' },
+		])
+	})
+	it('parses multiple conditions', ({ expect }) => {
+		expect(parse.parseConditionOption('foo:1,bar__gt:69,baz:29')).toEqual([
+			{ sql: 'foo', op: '=', params: '1' },
+			{ sql: 'bar', op: '>', params: '69' },
+			{ sql: 'baz', op: '=', params: '29' },
+		])
+	})
+})
+
+describe('parseConditionKeyVal', () => {
+	it('parses empty', ({ expect }) => {
+		expect(parse.parseConditionKeyVal('', '')).toEqual(null)
+		expect(parse.parseConditionKeyVal('a', '')).toEqual(null)
+	})
+	it('parses condition', ({ expect }) => {
+		expect(parse.parseConditionKeyVal('foo__gt', '69')).toEqual({
+			sql: 'foo',
+			op: '>',
+			params: '69',
+		})
+	})
+})
+
+describe('parseJoinOption', () => {
+	it('parses empty', ({ expect }) => {
+		expect(parse.parseJoinOption('')).toEqual(null)
+		expect(parse.parseJoinOption(':')).toEqual(null)
+	})
+
+	it('parses join', ({ expect }) => {
+		expect(parse.parseJoinOption('foo')).toEqual({ join: 'foo' })
+	})
+
+	it('parses with names', ({ expect }) => {
+		expect(parse.parseJoinOption('foo:bar')).toEqual({
+			join: 'foo',
+			name: 'bar',
+		})
+		expect(parse.parseJoinOption('foo:bar:baz')).toEqual({
+			join: 'foo',
+			name: 'bar',
+			col: 'baz',
+		})
+		expect(parse.parseJoinOption('foo:bar:baz:boom')).toEqual({
+			join: 'foo',
+			name: 'bar',
+			col: 'baz',
+			table: 'boom',
+		})
+	})
+})
+
+describe('parseSortOption', () => {
+	it('parses empty', ({ expect }) => {
+		expect(parse.parseSortOption('')).toEqual(null)
+		expect(parse.parseSortOption('-')).toEqual(null)
+		expect(parse.parseSortOption('^')).toEqual(null)
+	})
+
+	it('parses asc', ({ expect }) => {
+		expect(parse.parseSortOption('foo')).toEqual({ col: 'foo' })
+	})
+
+	it('parses desc', ({ expect }) => {
+		expect(parse.parseSortOption('-foo')).toEqual({ col: 'foo', desc: true })
+	})
+
+	it('parses asc nulls first', ({ expect }) => {
+		expect(parse.parseSortOption('^foo')).toEqual({
+			col: 'foo',
+			nullsFirst: true,
+		})
+	})
+
+	it('parses desc nulls first', ({ expect }) => {
+		expect(parse.parseSortOption('_foo')).toEqual({
+			col: 'foo',
+			desc: true,
+			nullsFirst: true,
+		})
+	})
+})
+
 describe('parsePagination', () => {
 	it('parses limit', ({ expect }) => {
 		expect(parse.parsePagination(usp('?_l='))).toEqual({})
@@ -37,5 +172,62 @@ describe('parsePagination', () => {
 			skip: 50,
 			take: 25,
 		})
+	})
+})
+
+describe('parseInsertItems', () => {
+	it('parses correctly', ({ expect }) => {
+		expect(parse.parseInsertItems([])).toEqual([])
+		expect(parse.parseInsertItems([{ foo: 'bar', baz: 'boom' }])).toEqual([
+			{
+				cols: ['baz', 'foo'],
+				values: [['boom', 'bar']],
+			},
+		])
+		expect(
+			parse.parseInsertItems([
+				{ a: 1, b: 2 },
+				{ a: 3, b: 4 },
+			])
+		).toEqual([
+			{
+				cols: ['a', 'b'],
+				values: [
+					[1, 2],
+					[3, 4],
+				],
+			},
+		])
+	})
+
+	it('parses for different keys', ({ expect }) => {
+		expect(
+			parse.parseInsertItems([
+				{ a: 1, b: 2 },
+				{ a: 3, b: 4, c: 5 },
+				{ a: 6, b: 7 },
+				{ a: 8, b: 9, c: 10 },
+				{ a: 11, c: 12 },
+			])
+		).toEqual([
+			{
+				cols: ['a', 'b'],
+				values: [
+					[1, 2],
+					[6, 7],
+				],
+			},
+			{
+				cols: ['a', 'b', 'c'],
+				values: [
+					[3, 4, 5],
+					[8, 9, 10],
+				],
+			},
+			{
+				cols: ['a', 'c'],
+				values: [[11, 12]],
+			},
+		])
 	})
 })
